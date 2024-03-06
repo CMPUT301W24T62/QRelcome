c6090298eb69727dc50915b913eb861645a7e4af
Pooja Prasad <pprasad1@ualberta.ca>
2024-03-06 13:39:34 -0700

    Updated QRCodeScanner

--- app/src/main/java/com/example/qrelcome/QRCodeScanner.java
+++ app/src/main/java/com/example/qrelcome/QRCodeScanner.java
@@ -50,7 +50,6 @@ public class QRCodeScanner extends AppCompatActivity implements View.OnClickList
      *     previously being shut down then this Bundle contains the data it most
      *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
      */
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
